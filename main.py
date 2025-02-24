241be9c56c43a0f1090c841b95b40edf80be4121
ChurongDavidWei <c_wei3@mail.fhsu.edu>
2025-02-23 21:20:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ Assignment # mini project 2
 I,     Churong'David'Wei    , affirm that the work submitted for this assignment is entirely my own. I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
 '''
 
+#Class: INF601_VA
+#Name: Churong"David"Wei
+#Project: miniproject2
+
 # Import all needed libraries
 import pandas as pd
 import matplotlib.pyplot as plt
